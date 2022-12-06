bf9bc05eb843a4195a45d22d6311a26675d2e601
Mytrykar <ipushka1996@gmail.com>
2022-12-06 15:37:18 +0200

    .

--- lib/src/template/widgets/example_bloc_widget.dart/bloc/provider.dart
+++ lib/src/template/widgets/example_bloc_widget.dart/bloc/provider.dart
@@ -3,6 +3,9 @@ import 'package:mason_app_project/src/template/bloc_widget_template.dart';
 
 extension ProviderTemplate on BlocWidgetTemplate {
   String get templateProvider {
+    final String import = modelName != null
+        ? "import 'package:${projectName.toSnakeCase()}/app/models/${modelName!.toSnakeCase()}/${modelName!.toSnakeCase()}_model.dart"
+        : "";
     final String model =
         modelName != null ? "${modelName!.toPascalCase()}? _data;" : "";
     final a = modelName != null
@@ -10,15 +13,25 @@ extension ProviderTemplate on BlocWidgetTemplate {
 Future<${modelName!.toPascalCase()}>? get data async{
   //TODO GET in ServiseApi
   // final response = await ApiServise.get();
-  // 
+  // _data = response;
+  // return _data;
   return null;
 }
 """
         : "";
+    final b = modelName != null
+        ? """
+${modelName!.toPascalCase()}? get current => _data;
+"""
+        : "";
     return """
-class ${widgetName.toPascalCase()}Provider{
-  $model
 
+class ${widgetName.toPascalCase()}Provider{
+  // final apiServise = locator<ApiServise>;
+  // final localStorageServise = locator<LocalStorageServise>;
+  $model
+  $a
+  $b
 }
 """;
   }
